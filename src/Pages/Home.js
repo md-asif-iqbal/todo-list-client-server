95a47da58bcb420105229b1e3bbcb5c00a95ce59
Asif Iqbal <asifiqbal@Asifs-MBP.Dlink>
2022-05-19 07:37:21 +0600

    done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <div>
             <h2 className='text-5xl mt-10'>What's the Plan for Today?</h2>
             <h2 className='text-3xl mt-5 mb-10'>Please go to Todo List</h2>
-      <Link to='/todolist' ><button class="btn btn-primary mb-40">Click Here</button></Link>
+      <Link to='/todolist' ><button className="btn btn-primary mb-40">Click Here</button></Link>
             
         </div>
     );
